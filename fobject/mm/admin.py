7fb494b26a93fd9256a8be2dfdbcd86245ec68a6
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2017-10-31 13:27:14 -0600

    admin para todo

--- fobject/mm/admin.py
+++ fobject/mm/admin.py
@@ -3,10 +3,17 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Alter, Type, EgoEdge, Action
+from .models import Alter, Type, EgoEdge, Action, \
+    Sector, Agency, MentalType, Item, MentalEdge
 
 admin.site.register(Type)
+admin.site.register(Sector)
 admin.site.register(Alter)
+
 admin.site.register(EgoEdge)
 
 admin.site.register(Action)
+admin.site.register(Agency)
+admin.site.register(MentalType)
+admin.site.register(Item)
+admin.site.register(MentalEdge)
