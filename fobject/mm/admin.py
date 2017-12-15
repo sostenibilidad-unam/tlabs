77577b278a3f7b8ffcd998b6f09ebd185dc7cf93
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2017-12-15 13:17:39 -0600

    admins mas amigables

--- fobject/mm/admin.py
+++ fobject/mm/admin.py
@@ -6,10 +6,25 @@ from django.contrib import admin
 from .models import Alter, Type, EgoEdge, Action, \
     Sector, Agency, MentalType, Item, MentalEdge, Category
 
+
+class AlterAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name', 'sector']
+
+
+admin.site.register(Alter, AlterAdmin)
+
+
+class EgoEdgeAdmin(admin.ModelAdmin):
+    search_fields = ['source__name', 'target__name']
+    list_display = ['id', 'source', 'target', 'distance']
+
+
+admin.site.register(EgoEdge, EgoEdgeAdmin)
+
+
 admin.site.register(Type)
 admin.site.register(Sector)
-admin.site.register(Alter)
-admin.site.register(EgoEdge)
 
 admin.site.register(Category)
 admin.site.register(Action)
