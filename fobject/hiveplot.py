aaf04419459a7465d24198c1a4a760d5169bb694
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2017-12-30 18:58:14 -0600

    bigger labels, better placed

--- fobject/hiveplot.py
+++ fobject/hiveplot.py
@@ -100,7 +100,7 @@ for e in Alter.objects.filter(name__contains='TL0').order_by('degree').all():
     g = svgwrite.container.Group(transform='rotate(-90, %s, %s)' % (n.x, n.y),
                                  style='font-size:22')
     g.add(n.dwg.text(e.name,
-                     insert=(n.x, n.y)))
+                     insert=(n.x - 65, n.y)))
     n.dwg.add(g)
 
 
