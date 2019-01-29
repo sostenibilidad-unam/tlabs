820ed2df51cd33774de56baa0b85283c80005d1a
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2019-01-29 13:16:56 -0600

    hive plots for specific egos, phases

--- fobject/make_hives.sh
+++ fobject/make_hives.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-python hiveplot_actioncats_joined.py all 'baseline'
-python hiveplot_actioncats_joined.py all 'phase 3'
+python hiveplot_actioncats_joined.py --phase 'baseline'
+python hiveplot_actioncats_joined.py --phase 'post-earthquake'
+python hiveplot_actioncats_joined.py --phase 'phase 3'
 
-inkscape -D -e  agency_actioncats_joined_baseline.png agency_actioncats_joined_baseline.svg
-inkscape -D -e  agency_actioncats_joined_phase3.png agency_actioncats_joined_phase\ 3.svg
+python hiveplot_actioncats_joined.py --phase 'baseline' --egos TL010 TL016 TL014
+python hiveplot_actioncats_joined.py --phase 'post-earthquake' --egos TL010 TL016 TL014
+python hiveplot_actioncats_joined.py --phase 'phase-3' --egos TL010 TL016 TL014
+
+find . -iname 'ana_hiveplot*svg' -printf "inkscape -D -e \`basename %p .svg\`.png %p \n" | sh
