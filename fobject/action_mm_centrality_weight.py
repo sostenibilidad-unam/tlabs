294c6e515055a4990177d4f0c1564bb23c8fd4da
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2019-01-29 22:57:28 -0600

    b0rked heatmap, but dataframe looking good

--- fobject/action_mm_centrality_weight.py
+++ fobject/action_mm_centrality_weight.py
@@ -3,6 +3,13 @@ import sys
 import os
 import argparse
 import networkx as nx
+import pandas as pd
+import numpy as np
+#import seaborn as sns
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+
 
 parser = argparse.ArgumentParser(
     description="sum centrality weights for action")
@@ -22,9 +29,13 @@ if __name__ == '__main__':
     import django
     django.setup()
 
-    from mm.models import Alter, Phase
+    from mm.models import Alter, Phase, ActionEdge, Variable
 
 phase = Phase.objects.get(phase=args.phase)
+
+actions_centrality_sum = {action.action: {var.name: 0.0
+                                          for var in Variable.objects.all()}
+                          for action in ActionEdge.objects.filter(phase=phase)}
 
 
 def bag_of_actions(ego):
@@ -33,7 +44,49 @@ def bag_of_actions(ego):
                 for action in ego_edge.target.action_set.filter(phase=phase)]
 
 
+def upsert(dsum, acc, con, v):
+
+    if acc in dsum:
+        if con in dsum[acc]:
+            dsum[acc][con] += v
+        else:
+            dsum[acc][con] = v
+    else:
+        dsum[acc] = {con: v}
+
+
+all_actions = set([])
+all_vars = set([])
+
+# sum mental model variable centralities from Egos for all actions
 for ego in Alter.objects.filter(name__startswith='TL0'):
     g = ego.mental_model(phase)
-    print ego, nx.centrality.betweenness_centrality(g)
-    print ego, bag_of_actions(ego)
+    centralities = nx.centrality.betweenness_centrality(g)
+
+    for action in bag_of_actions(ego):
+        all_actions.add(action)
+        for variable in centralities:
+            all_vars.add(variable)
+            upsert(actions_centrality_sum,
+                   action,
+                   variable, centralities[variable])
+
+
+rows = {}
+
+for action in actions_centrality_sum:
+    if sum(actions_centrality_sum[action].values()) > 0:
+        rows[action] = actions_centrality_sum[action].values()
+
+
+df = pd.DataFrame.from_dict(rows,
+                            orient='index',
+                            columns=[unicode(v.name) for v in Variable.objects.all()])
+
+df.drop([v for v in df.columns if df[v].sum() == 0], axis=1)
+#sns.heatmap(df, annot=True)
+plt.pcolor(df)
+#plt.ylabels(df.index)
+plt.yticks(np.arange(0.5, len(df.index), 1), df.index)
+#plt.xticks(np.arange(0.5, len(df.columns), 1), df.columns)
+plt.show()
