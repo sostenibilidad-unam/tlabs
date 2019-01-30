31c6a4afaa8b87fa6e74597058301120f38c7f67
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2019-01-30 15:50:27 -0600

    render heatmap

--- fobject/action_mm_centrality_weight.py
+++ fobject/action_mm_centrality_weight.py
@@ -5,7 +5,7 @@ import argparse
 import networkx as nx
 import pandas as pd
 import numpy as np
-#import seaborn as sns
+import seaborn as sns
 import matplotlib.pyplot as plt
 # %matplotlib inline
 
@@ -75,18 +75,35 @@ for ego in Alter.objects.filter(name__startswith='TL0'):
 rows = {}
 
 for action in actions_centrality_sum:
-    if sum(actions_centrality_sum[action].values()) > 0:
-        rows[action] = actions_centrality_sum[action].values()
+    rows[action] = actions_centrality_sum[action].values()
 
 
 df = pd.DataFrame.from_dict(rows,
                             orient='index',
                             columns=[unicode(v.name) for v in Variable.objects.all()])
 
-df.drop([v for v in df.columns if df[v].sum() == 0], axis=1)
+df1 = df.drop([v for v in df.columns if df[v].sum() == 0], axis=1)
 #sns.heatmap(df, annot=True)
-plt.pcolor(df)
+#plt.pcolor(df1)
 #plt.ylabels(df.index)
-plt.yticks(np.arange(0.5, len(df.index), 1), df.index)
+#plt.yticks(0.5, df1.index)
 #plt.xticks(np.arange(0.5, len(df.columns), 1), df.columns)
-plt.show()
+#plt.show()
+
+
+cm = sns.clustermap(df1, cmap="Blues",
+                    yticklabels=[unicode(l) for l in df1.index],
+                    figsize=(20, 20))
+
+
+plt.setp(cm.ax_heatmap.yaxis.get_majorticklabels(), fontsize=7)
+plt.setp(cm.ax_heatmap.xaxis.get_majorticklabels(), fontsize=7)
+cm.cax.set_visible(False)
+
+# for t in cm.ax_heatmap.xaxis.get_majorticklabels():
+#     t.set_rotation_mode('anchor')
+#     t.set_rotation(60.0)
+
+
+plt.subplots_adjust(left=0, bottom=0.36, right=0.5, top=0.99, wspace=0, hspace=0)
+plt.savefig('heatmap_mmvars_actions.png')
